169eb318a9547b402c27afefa34f925e4aae1030
jrshor3 <112657230+jrshor3@users.noreply.github.com>
2023-06-09 10:40:53 -0400

    created shape number mapping in ToPandas

--- Summer-Research-2022/ToPandas.py
+++ Summer-Research-2022/ToPandas.py
@@ -4,6 +4,16 @@ class ToPandas():
     
     def ToPanda(JsonFile):
 
+        SHAPE_NUMBER_MAP = {"Line Segment": 00,
+                            "Isosceles Right Triangle": 10, "Right Triangle" : 11, "Equilateral Triangle" : 12,
+                                "Isosceles Triangle" : 13,
+                            "Square" : 20, "Rectangle" : 21, "Rhombus" : 22, "Parallelogram" : 23, "Kite" : 24,
+                                "Right Trapezoid" : 25, "Isosceles Trapezoid" : 26, "Dart" : 27,
+                            "Regular Pentagon" : 30,
+                            "Regular Hexagon" : 40,
+                            "Regular Septagon" : 50,
+                            "Regular Octogon" : 60}
+        
         with open(JsonFile,'r') as f:
             data = json.loads(f.read())
 
@@ -16,10 +26,11 @@ class ToPandas():
         for i, num in enumerate(edges['count'].values):
             for j in range(num-1):
                 edges.loc[len(edges.index)] = edges.iloc[i]
-        edges = edges.drop("count", axis=1) 
+        edges = edges.drop("count", axis=1)
 
         nodes = nodes.rename(nodes["ID"])
         nodes = nodes.drop("ID", axis=1)
+        nodes = nodes["shape"].replace(SHAPE_NUMBER_MAP)
 
 
         return edges, nodes
