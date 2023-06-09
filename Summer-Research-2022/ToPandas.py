c04171c2391be5aa11fffc77bddcf87e7e8bedab
jrshor3 <112657230+jrshor3@users.noreply.github.com>
2023-06-09 11:33:39 -0400

    added comments

--- Summer-Research-2022/ToPandas.py
+++ Summer-Research-2022/ToPandas.py
@@ -1,9 +1,12 @@
 import pandas as pd
 import json
+
 class ToPandas():
     
+    # converts an appropriate JSON file into an equivalent Panda DataFrame
     def ToPanda(JsonFile):
 
+        # mapping that shows how the shape names found in the JSON are to be converted into a numerical representation
         SHAPE_NUMBER_MAP = {"Line Segment": 00,
                             "Isosceles Right Triangle": 10, "Right Triangle" : 11, "Equilateral Triangle" : 12,
                                 "Isosceles Triangle" : 13,
@@ -14,26 +17,28 @@ class ToPandas():
                             "Regular Septagon" : 50,
                             "Regular Octogon" : 60}
         
+        # reads the edge and node data from the JSON into Pandas
         with open(JsonFile,'r') as f:
             data = json.loads(f.read())
-
         try:
             edges = pd.json_normalize(data, record_path=['edges'],errors='ignore')
             nodes = pd.json_normalize(data, record_path=['nodes'],errors='ignore')
         except KeyError as e:
             print(f"Unable to normalize json: {json.dumps(data, indent=4)}")
 
+        # adds multiedges
         for i, num in enumerate(edges['count'].values):
             for j in range(num-1):
                 edges.loc[len(edges.index)] = edges.iloc[i]
         edges = edges.drop("count", axis=1)
 
+        # converts the shape IDs to row labels
         nodes = nodes.rename(nodes["ID"])
         nodes = nodes.drop("ID", axis=1)
+
+        # converts the shape names into numerical representation
         nodes["shape"].replace(SHAPE_NUMBER_MAP, inplace=True)
-        if (~nodes["shape"].isin(SHAPE_NUMBER_MAP)).sum() != 0:
+        if (~nodes["shape"].isin(SHAPE_NUMBER_MAP.values())).sum() != 0:
             raise Exception("Invalid Shape")
 
         return edges, nodes
-    
-    print(ToPanda('C:/Users/awgar/Desktop/JsonShapes/ConcaveTriangles.json'))
