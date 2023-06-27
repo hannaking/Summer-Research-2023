3493edec8f80a4ae0467887dd695e64615996c1f
mandatory-username <106693160+hannaking@users.noreply.github.com>
2023-06-27 15:13:54 -0400

    pentagon update

--- Summer-Research-2022/shapes/pentagon/pentagon.py
+++ Summer-Research-2022/shapes/pentagon/pentagon.py
@@ -10,10 +10,9 @@ from shapes.vector import Vector
 ANGLE = math.radians(108)
 DEFAULT_SIDE_LENGTH = 1
 
-# _points = a list of Point objects that represent the starting position for the equilateral triangle
+# _points = a list of Point objects that represent the starting position for the regular pentagon
 
 # 5 sides of equal length, all angles are 108 degrees
-#TODO: unfinished. the Pentagon class must be able to take anywhere between 0-5 points inclusive and generate a valid Pentagon from them.
 
 class Pentagon(): 
 
@@ -35,40 +34,34 @@ class Pentagon():
         sorted_points = [b[1] for b in first_sort]
 
         side_length = DEFAULT_SIDE_LENGTH
+        second_points = sorted_points[1]
+        third_points = sorted_points[2]
+        fourth_points = sorted_points[3]
+        fifth_points = sorted_points[4]
 
-        second_points = []
         # get all possible next point for the given single point
         if (sorted_points[1] == None): # one known point
+            second_points = []
             second_points.extend(Geometry.get_second_points(sorted_points[0]))
-        else: # two known points
-            second_points.append(sorted_points[1])
-            side_length = Geometry.distance(sorted_points[0], sorted_points[1])
 
-        # now, there is only one possible third point for each second point
-        # 108 degrees off
-        third_points = []
-        for point2 in second_points:
-            third_points.extend(Geometry.calculate_point_from_angle(ANGLE, sorted_points[0], point2, side_length))
+        # now have at least 2 points, so side length can be determined
+        side_length = Geometry.get_distance(sorted_points[0], sorted_points[1])
 
-        # now, there is only one possible fourth point for each third point
-        # 108 degrees off
-        fourth_points = []
-        for i in range(0, len(third_points)):
-            fourth_points.extend(Geometry.calculate_point_from_angle(ANGLE, second_points[i], third_points[i], side_length))
-
-        # now, there is only one possible fifth point for each fourth point
-        # 108 degrees off
-        fifth_points = []
-        for i in range(0, len(fourth_points)):
-            fifth_points.extend(Geometry.calculate_point_from_angle(ANGLE, third_points[i], fourth_points[i], side_length))
-
-        # now I have...
-        # first point -> sorted_points[0]
-        # second point -> from list second_points, which has len 10
-        # third point -> from list third_points, which has len 10 to match
-        # fourth point -> from list fourth_points, which has len 10 to match
-        # fifth point -> from list fifth_points, which has len 10 to match
-
+        # then continue to the next
+        if  (sorted_points[2] == None): # two known points
+            third_points = []
+            third_points.extend(self.get_next_points(second_points, sorted_points[0], side_length))
+            
+        # then continue to the next
+        if  (sorted_points[3] == None): # three known points
+            fourth_points = []
+            fourth_points.extend(self.get_next_points(third_points, second_points, side_length))
+        # then continue to the next
+        if (sorted_points[4] == None): # four known points
+            fifth_points = []
+            fifth_points.extend(self.get_next_points(fourth_points, third_points, side_length))
+        # all five points known handled above
+        
         # build the scenarios
         for i in range(0, len(fifth_points)):
             scenario = []
@@ -86,3 +79,11 @@ class Pentagon():
 
         # a list of lists of 5 Points
         return scenarios
+    
+    # reference points are the points one back. so if i'm looking for the 3rd points,
+    # points is the second points and reference the first
+    def get_next_points(self, points, reference_points, length):
+        new_points = []
+        for i in range(0, len(points)):
+            new_points.extend(Geometry.calculate_point_from_angle(ANGLE, points[i], reference_points[i], length))
+        return new_points
