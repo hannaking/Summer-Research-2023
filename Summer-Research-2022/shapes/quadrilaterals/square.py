f9e9e77e0f231f1a22495c2e913b622c0a220e1e
HKing <106693160+hannaking@users.noreply.github.com>
2023-07-27 14:56:47 -0400

    square fix and test outline

--- Summer-Research-2022/shapes/quadrilaterals/square.py
+++ Summer-Research-2022/shapes/quadrilaterals/square.py
@@ -11,6 +11,15 @@ from vector import Vector
 
 ANGLE = math.radians(90)
 DEFAULT_SIDE_LENGTH = 1
+ROTATE_ANGLES = [math.radians(30),
+                 math.radians(45),
+                 math.radians(60),
+                 math.radians(90),
+                 math.radians(180),
+                 math.radians(-30),
+                 math.radians(-45),
+                 math.radians(-60),
+                 math.radians(-90)]
 
 # _points = a list of Point objects that represent the starting position for the equilateral triangle
 
@@ -21,151 +30,137 @@ class Square():
         self._points = known_coords
 
     def coordinatize(self):
-        # you will start with either one point, two points, or three points.
-        #
-        # First, we sort the coordinates #?
-        #
-        # if you have one point, you will need to find the second point. There is only one option: 1 unit to the right of the start point.
-        # We make this decision based on the fact that since we only know the first point, we have no knowledge of the size of the square,
-        # so we default to 1 unit. It is to the right because it would look the same whether it was to the left, up, or down, so it does not make
-        # much of a meaningful difference.
-        #
-        # once two points are found, you will need to find the third points (plural!) 
-        # If you were given two points to start with, you find the distance between the two. 
-        # This is the side length of the square, so you know how far away the third point should be.
-        # There are two options: above the second point and below the second point.
-        #
-        # Once three points are found, you will need to find the fourth points.
-        # There is only one option: the final corner of the square.
-        # However, you need to repeat the process for each third coordinate.
-        # Like before, find the side length by finding the distance between two of the points.
-        # Then you'll find the points that are 90 and -90 degrees from the line formed by the third point and the second point.
-        # Only one of these coordinates is correct.
-        # We find the correct point by creating vectors between the first point and the second point, then the first point and the two possible third points.
-        # We check which of the vectors are orthogonal to the vector between the first point and the second point.
-        # The one that is orthogonal is the correct point.
-        # The one that is not orthogonal is the incorrect point.
-        #
-        # Once you have the fourth point, you have a complete scenario.
-        #
-        # For each of these scenarios, we rotate the points about the origin with the angles we care about:
-        # [30, 45, 60, 90, 180 ,-30, -45 ,-60 ,-90] degrees (will convert to radians)
-        # Each of these is a new scenario.
-        # We then unsort these scenarios, then return them.
+        scenarios = []
 
-        scenarios = []  # list of lists of Points
-
-        # I need to sort the coords , dragging along a list of indices.
-        first_sort = [ b for b in sorted(enumerate(self._points), key=lambda e: e[1] is None ) ]
-
-        # but this does get a sorted points list
+        # sorts to be traversible in order
+        first_sort = [ b for b in sorted(enumerate(self._points), key=lambda e: e[1] is None)]
         sorted_points = [b[1] for b in first_sort]
 
-        # to make it easier to understand what sorted points are
+        # checks if there are the correct number of points
+        if len(sorted_points) != 5:
+            return []
+
         point1 = sorted_points[0]
         point2 = sorted_points[1]
         point3 = sorted_points[2]
         point4 = sorted_points[3]
 
-        # checks if a parallelogram can't be made or if the passed in points are already a parallelogram
+        vertex_gluing = False
+
+        scenarios = [[point1, point2, point3, point4]]
+            
+        # 4 points in
+        # checks if a square can't be made or if the passed in points are already a square
         if None not in sorted_points:
             if(self._verify_square()):
                 return [self._points]
             else:
                 return []
 
-        # boolean that tells whether this square is vertex glued or not. default to False.
-        vertex_gluing = False
-
         if point2 == None:
-            # get second point
-            point2 = self.get_second_point(point1)
-            # since we only have one point, we know that the square is vertex glued.
+            scenarios[0][1] = self.get_second_point(point1)
             vertex_gluing = True
-
-        if point3 == None:
-            # get third points 
-            third_points = self.get_third_points(point1, point2)
         
-        if point4 == None:
-            if(point3 != None and not self._verify_square_3_points()):
+        if point3 == None:
+            scenarios = self.get_third_points(scenarios[0][0], scenarios[0][1])
+            # now scenarios has len 2
+        else:
+            # checks if the known n points can even form a square
+            if(not self._verify_square_3_points()):
                 return []
-            # get fourth points
-            fourth_points = self.get_fourth_points(point1, point2)
 
-        # we have two scenarios at 0 degrees: above the x axis and below the x axis
-        scenarios.append([point1, point2, third_points[0], fourth_points[0]])
-        scenarios.append([point1, point2, third_points[1], fourth_points[1]])
+        if point4 == None:            
+            scenarios = self.get_next_point_scenarios(scenarios, 3)
 
-        # you would only ever want to rotate your shape if you are vertex glued.
-        # if you are already given two or three points, there is no point in rotating your shape.
+        # makes all of the rotations around point1
         if vertex_gluing == True:
-            # now we will rotate each scenario by [30,45,60,90,180,-30-45,-60,-90] degrees (will convert to radians),
-            # creating a new scenario, and add it to the list of scenarios
-            # 2 scenarios * 9 angles = 18 new scenarios
-            # 20 scenarios in total
-            # note: we are rotating the points about point 1, because we know that point 1 is either the origin or the vertex we are glued to.
-            angles = [30, 45, 60, 90, 180, -30, -45, -60, -90]
-            original_scenario_len = len(scenarios)
-            for i in range(original_scenario_len):
-                for angle in angles:
-                    new_scenario = Geometry.rotate(scenarios[i], math.radians(angle))
-                    scenarios.append(new_scenario)
+            scenarios = self.get_rotated_scenarios(scenarios)
 
-        # unsort all scenarios
+        # put back in lattice order
         for i, scenario in enumerate(scenarios):
             scenario = [b[1] for b in sorted(zip(first_sort, scenario), key=lambda e: e[0][0])]
             scenarios[i] = scenario
 
-        # a list of lists of 4 Points
         return scenarios
-
+    
     # returns a Point object. there is only one option: 1 unit to the right of the start point.
     def get_second_point(self, point1):
         return Point(point1.x + DEFAULT_SIDE_LENGTH, point1.y)
 
-    # return a list of Point objects. 
-    # finds the third points by finding the point 90 degrees and -90 degrees from the line formed by pt 1 and pt 2
+    # gets the scenarios that include the third points from the scenario including
+    # only the first point and the second point.
+    # 
+    # scenarios - list of list of one point and three Nones
+    # 
+    # returns the new scenario
+    def get_third_point_scenarios(self, scenarios):
+        new_scenarios = []
+        for scenario in scenarios:
+            point1 = scenario[0]
+            point2 = scenario[1]
+
+            third_points = self.get_third_points(point1, point2)
+            
+            for third_point in third_points:
+                new_scenarios.append([point1, point2, third_point, None])
+
+        return new_scenarios
+    
+    # gets the third points given two points
+    #
+    # point1 - 2d point
+    # point2 - 2d point
+    #
+    # returns the third and fourth points
     def get_third_points(self, point1, point2):
-        side_length = Geometry.distance(point1, point2)
         third_points = []
+        side_length = Geometry.distance(point1, point2)
+
         third_points.append(Geometry.calculate_point_from_angle(ANGLE, point2, point1, side_length))
         third_points.append(Geometry.calculate_point_from_angle(-ANGLE, point2, point1, side_length))
-
+        
         return third_points
 
-    # return a Point object. there is 1 option: the final corner of the square.
-    # finds the fourth points by finding the point -90 degrees and 90 degrees from the line formed by pt 2 and pt 1
-    # notice how this is FLIPPED from get_third_points.
-    def get_fourth_points(self, point1, point2):
-        side_length = Geometry.distance(point1, point2)
-        fourth_points = []
-        fourth_points.append(Geometry.calculate_point_from_angle(-ANGLE, point1, point2, side_length))
-        fourth_points.append(Geometry.calculate_point_from_angle(ANGLE, point1, point2, side_length))
+    # gets the scenarios that include the next points from the scenarios including
+    # 
+    # scenarios - list of list of one point and three Nones
+    # place - int, the index of the new point
+    # 
+    # returns the new scenario
+    def get_next_point_scenarios(self, scenarios, place):
+        new_scenarios = []
+        for scenario in scenarios:
+            point_prev3 = scenario[place - 3]
+            point_prev2 = scenario[place - 2]
+            point_prev1 = scenario[place - 1]
 
-        return fourth_points
-    
-    # gets the angle in degrees between three points
-    #  *angle is signed and between (-pi, pi) radians
-    #
-    # point1 - 2d point | one side of the angle
-    # mid_point - 2d point | mid point of the angle
-    # point3 - 2d point | other side of the angle
-    #
-    # returns the angle in radians
-    @staticmethod
-    def get_angle(point1, mid_point, point3):
-        a = np.array([point1.x, point1.y])
-        b = np.array([mid_point.x, mid_point.y])
-        c = np.array([point3.x, point3.y])
-
-        ba = a - b
-        bc = c - b
+            new_point = self.get_next_point(point_prev3, point_prev2, point_prev1)
+            
+            new_scenario = []
+            for i, point in enumerate(scenario):
+                if i == place:
+                    new_scenario.append(new_point)
+                else:
+                    new_scenario.append(point)
+            new_scenarios.append(new_scenario.copy())
         
-        angle = math.atan2(ba[0] * bc[1] - ba[1] * bc[0],
-                           ba[0] * bc[0] + ba[1] * bc[1])
+        return new_scenarios
 
-        return angle
+    # gets the next point given the prior three points
+    #
+    # point_prev3 - 2d point | point before the point before the prior point
+    # point_prev2 - 2d point | point before the prior point
+    # point_prev1 - 2d point | prior point
+    #
+    # returns the fourth point
+    def get_next_point(self, point_prev3, point_prev2, point_prev1):
+        side_length = Geometry.distance(point_prev3, point_prev2)
+
+        angle = Geometry.get_angle(point_prev3, point_prev2, point_prev1)
+
+        next_point = Geometry.calculate_point_from_angle(angle, point_prev1, point_prev2, side_length)
+
+        return next_point
 
     def _verify_square_3_points(self):
         if len(self._points) != 4:
@@ -186,10 +181,22 @@ class Square():
             
         angle = abs(Geometry.get_angle(point1, point2, point3))
 
-        if not math.isclose(angle, math.pi / 2, abs_tol=1e-9) or not math.isclose(side1, side2, abs_tol=1e-9):
+        if not math.isclose(abs(angle), math.pi / 2, abs_tol=1e-9) or not math.isclose(side1, side2, abs_tol=1e-9):
             return False
             
         return True
+
+    # gets 10 scenarios rotated around the first point
+    #
+    # scenarios - the non-rotated scenarios 
+    #
+    # returns the rotated scenarios
+    def get_rotated_scenarios(self, scenarios):
+        new_scenarios = scenarios.copy()
+        for scenario in scenarios:
+            for angle in ROTATE_ANGLES:
+                new_scenarios.append(Geometry.rotate(scenario, angle))
+        return new_scenarios
     
     def _verify_square(self):
         if len(self._points) != 4:
--- Summer-Research-2022/shapes/unit_tests/square_unit_test.py
+++ Summer-Research-2022/shapes/unit_tests/square_unit_test.py
@@ -17,46 +17,87 @@ sys.path.append('Summer-Research-2022')
 from lattice import Lattice
 
 class TestSquare(unittest.TestCase):
-
-    def test_verify_square(self):
+    #-----------------------------------------init---------------------------------
+    def test_square(self):
         coords = [Point(0, 0), Point(1, 0), Point(1, 1), Point(0, 1)]
         shape = Square(coords)
-        self.assertTrue(shape._verify_square())
+        self.assertEqual(shape._points, coords)
 
-    def test_verify_square2(self):
-        coords = [Point(1, 1), Point(6, 1), Point(6, 6), Point(1, 6)]
-        shape = Square(coords)
-        self.assertTrue(shape._verify_square())
+    #-------------------------------------coordinatize-----------------------------
+    # 4 in - not a square
+    # 4 in - a square
+    # 3 in - can't make a square
+    # 3 in - can make a square
+    # 2 in
+    # 1 in
 
-    def test_verify_square3(self):
-        coords = [Point(1, 1), Point(6, 1), Point(6, 6), Point(1, 6)]
-        shape = Square(coords)
-        self.assertTrue(shape._verify_square())
+    #-------------------------------------get second point-------------------------
+    # normal
+    def test_get_second_point(self):
+        point1 = Point(0,0)
+        sq = Square([point1, None, None, None])
+        point2 = sq.get_second_point(point1)
+        self.assertEqual(point2, Point(1,0))
+
+    #----------------------------------verify square 3 points----------------------
+    # not 4 points - too many, too few
+    # point is None - 1, 2, 3
+    # point 4 is None
+    # sides not the same
+    # existing angle not 90 or -90
+    # can be a square
+
+    #--------------------------------------verify square---------------------------
+    # not 4 points - too many, too few
+    # any point is None
+    # side 1 and 3 don't match
+    # side 2 and 4 don't match
+    # angle not 90 or -90
+    # correct
+
+    # the rest is tested in pentagon unit tests, no need to repeat
+
+
+
+    #def test_verify_square(self):
+    #    coords = [Point(0, 0), Point(1, 0), Point(1, 1), Point(0, 1)]
+    #    shape = Square(coords)
+    ##    self.assertTrue(shape._verify_square())
+
+    #def test_verify_square2(self):
+    #    coords = [Point(1, 1), Point(6, 1), Point(6, 6), Point(1, 6)]
+    #    shape = Square(coords)
+    #    self.assertTrue(shape._verify_square())
+
+    #def test_verify_square3(self):
+    #    coords = [Point(1, 1), Point(6, 1), Point(6, 6), Point(1, 6)]
+    #    shape = Square(coords)
+    #    self.assertTrue(shape._verify_square())
     
-    def test_verify_square4(self):
-        coords = [Point(-2, -3), Point(1, -3), Point(1, 4), Point(-2, 4)]
-        shape = Square(coords)
-        self.assertTrue(shape._verify_square())
+    #def test_verify_square4(self):
+    #    coords = [Point(-2, -3), Point(1, -3), Point(1, 4), Point(-2, 4)]
+    #    shape = Square(coords)
+    #    self.assertTrue(shape._verify_square())
 
-    def test_verify_square_too_many_coords(self):
-        coords = [Point(0, 0), Point(1, 0), Point(1, 1), Point(0, 1), Point(2, 2)]
-        shape = Square(coords)
-        self.assertFalse(shape._verify_square())
+    #def test_verify_square_too_many_coords(self):
+    #    coords = [Point(0, 0), Point(1, 0), Point(1, 1), Point(0, 1), Point(2, 2)]
+    #    shape = Square(coords)
+    #    self.assertFalse(shape._verify_square())
 
-    def test_verify_square_too_few_coords(self):
-        coords = [Point(0, 0), Point(1, 0), Point(1, 1)]
-        shape = Square(coords)
-        self.assertFalse(shape._verify_square())
+    #def test_verify_square_too_few_coords(self):
+    #    coords = [Point(0, 0), Point(1, 0), Point(1, 1)]
+    #    shape = Square(coords)
+    #    self.assertFalse(shape._verify_square())
 
-    def test_verify_square_not_rectangle(self):
-        coords = [Point(0, 0), Point(1, 0), Point(0, 1)]
-        shape = Square(coords)
-        self.assertFalse(shape._verify_square())
+    #def test_verify_square_not_rectangle(self):
+    #    coords = [Point(0, 0), Point(1, 0), Point(0, 1)]
+    #    shape = Square(coords)
+    #    self.assertFalse(shape._verify_square())
 
-    def test_verify_square_not_rectangle_2(self):
-        coords = [Point(2, 4), Point(0, 5), Point(5, 6), Point(3, 3)]
-        shape = Square(coords)
-        self.assertFalse(shape._verify_square())
+    #def test_verify_square_not_rectangle_2(self):
+    #    coords = [Point(2, 4), Point(0, 5), Point(5, 6), Point(3, 3)]
+    #    shape = Square(coords)
+    #    self.assertFalse(shape._verify_square())
 
     #
     def test_(self):
